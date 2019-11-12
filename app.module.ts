4cb50e156fdfb5052dd94b46c0fd326644ea4098
rpieri <raphaeldppf@hotmail.com>
2019-11-12 17:32:24 -0300

    Initial commit

---
+++ app.module.ts
@@ -0,0 +1,37 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsSingleSelectExampleComponent } from './src/forms-single-select-example.component'
+
+@Component({
+    selector: 'app-component',
+    template: '<forms-single-select-example></forms-single-select-example>'
+})
+export class AppComponent {}
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgSelectModule,
+        NgOptionHighlightModule,
+        CommonModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    declarations: [
+        AppComponent,
+        FormsSingleSelectExampleComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+
+
